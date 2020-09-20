f14fe1d5758c068fc6bc23f427178749bdd352eb
rsunder <rajashyam.sunder@dxc.com>
2020-09-20 17:20:52 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,4 @@ git clone https://github.dxc.com/Digital-Innovation-Lab/inp-deep-fake-detection.
 cd inp-deep-fake-detection
 conda install pip
 pip install -r requirements.txt
-cd anaconda3/envs/pytorch_latest_p36/lib/python3.6/site-packages/torch/
 jupyter notebook
