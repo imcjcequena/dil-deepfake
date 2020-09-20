c7a97a329268be013d0627ed778bd630c4a4048b
rsunder <rajashyam.sunder@dxc.com>
2020-09-20 16:20:46 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
-mkdir deepfake
+source activate pytorch_latest_p36
 cd deepfake
 git clone https://github.dxc.com/Digital-Innovation-Lab/inp-deep-fake-detection.git
 cd inp-deep-fake-detection
-pip3 install virtualenv
-python3 -m virtualenv venv
-. venv/bin/activate
-pip install --upgrade pip
-pip install -r requirement.txt
+conda install pip
+pip install -r requirements.txt
+cd anaconda3/envs/pytorch_latest_p36/lib/python3.6/site-packages/torch/
 jupyter notebook
