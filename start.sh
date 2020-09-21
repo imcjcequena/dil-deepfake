af8af48d2e999be63dded2e9edb3ca2f1e2fa49c
rsunder <rajashyam.sunder@dxc.com>
2020-09-21 13:00:59 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 source activate pytorch_latest_p36
+mkdir deepfake
 cd deepfake
 git clone https://github.dxc.com/Digital-Innovation-Lab/inp-deep-fake-detection.git
 cd inp-deep-fake-detection
-conda install pip
 pip install -r requirements.txt
 jupyter notebook
